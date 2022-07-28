ae68fe7ee3b956d952058e9c7451b3582157f4f0
ramapithecus <92436221+ramapithecus@users.noreply.github.com>
2022-07-28 22:32:32 +0200

    Add files via upload
    got few new ideas after watching badminton tournament on TV while i was work unable, after i got better i decided to implement more changes to this programm.
    f.i. interval break, redo score, set counts, prompt user to name team players( doubles playing), chose singles or doubles. Let's get to work

--- mainBSC.js
+++ mainBSC.js
@@ -1,5 +1,5 @@
 var body = document.body;
-body.classList.add('green');
+body.classList.add('theme');
 var gridContainer = document.createElement('div');
 body.appendChild(gridContainer);
 //head
@@ -9,36 +9,50 @@ gridContainer.appendChild(headContainer);
 //heading
 var para1T = document.createElement('p');
 para1T.innerHTML = 'Team1 :';
-var span1Score = document.createElement('span');
-span1Score.setAttribute('id','score1');
+var span1Score = document.createElement('p');
+span1Score.setAttribute('id', 'score1');
 span1Score.innerHTML = parseInt(0);
+span1Score.addEventListener('change', negativeScoreAlert);
 headContainer.appendChild(para1T);
-headContainer.appendChild(span1Score);  
+headContainer.appendChild(span1Score); 
 var para2T = document.createElement('p');
-para2T.innerHTML= 'Team2 :';
+para2T.innerHTML = 'Team2 :';
 headContainer.appendChild(para2T);
 var span2Score = document.createElement('span');
-span2Score.setAttribute('id','score2')
+span2Score.setAttribute('id', 'score2')
 span2Score.innerHTML = parseInt(0);
 headContainer.appendChild(span2Score);
 //buttons
 var btn1T = document.createElement('button');
 btn1T.classList.add('butt');
-btn1T.innerText ='Team1';
+btn1T.classList.add('pointbutt');
+btn1T.innerText = 'Team1';
 headContainer.appendChild(btn1T);
-btn1T.addEventListener('click',addScore1T);
-btn1T.addEventListener('click',winnerOfSet);
+btn1T.addEventListener('click', addScore1T);
+btn1T.addEventListener('click', winnerOfSet);
 var btn2T = document.createElement('button');
 btn2T.classList.add('butt');
+btn2T.classList.add('pointbutt');
 btn2T.innerText = 'Team2';
-btn2T.addEventListener('click',addScore2T);
-btn2T.addEventListener('click',winnerOfSet);
+btn2T.addEventListener('click', addScore2T);
+btn2T.addEventListener('click', winnerOfSet);
 headContainer.appendChild(btn2T);
-
+var btn1TRed = document.createElement('button');
+btn1TRed.classList.add('butt');
+btn1TRed.addEventListener('click', reduceScore1T);
+btn1TRed.addEventListener('click', negativeScoreAlert);
+headContainer.appendChild(btn1TRed);
+btn1TRed.innerText = 'point ↓T1';
+var btn2TRed = document.createElement('button');
+btn2TRed.classList.add('butt');
+btn2TRed.addEventListener('click', reduceScore2T);
+btn2TRed.addEventListener('click', negativeScoreAlert);
+headContainer.appendChild(btn2TRed);
+btn2TRed.innerText = 'point ↓ T2 ';
 var btnStart = document.createElement('button');
 btnStart.classList.add('startButt');
-btnStart.setAttribute('id','starterButt');
-btnStart.innerText = `let's start`;
+btnStart.setAttribute('id', 'starterButt');
+btnStart.innerText = `let's start `;
 btnStart.addEventListener('click', cockCreate);
 headContainer.appendChild(btnStart);
 
@@ -81,13 +95,51 @@ function addScore2T(){
     player3.appendChild(cockShuttle);
   }
 }
+function reduceScore1T(){
+    var scoreSpan = document.querySelector('#score1');
+    var cockShuttle = document.querySelector('#cock');
+    var scoreReducer = parseInt(scoreSpan.innerHTML);
+    scoreReducer = parseInt(scoreReducer)-1;
+    scoreSpan.innerHTML = parseInt(scoreReducer);
+     if (parseInt(scoreReducer) % 2 == 0) {
+      player1.appendChild(cockShuttle);
+    }
+    else {
+      player2.appendChild(cockShuttle);
+    }
+   
+}
+function reduceScore2T(){
+    var scoreSpan = document.querySelector('#score2');
+    var cockShuttle = document.querySelector('#cock');
+    var scoreReducer = parseInt(scoreSpan.innerHTML);
+    scoreReducer = parseInt(scoreReducer)-1;
+    scoreSpan.innerHTML = parseInt(scoreReducer);
+     if (parseInt(scoreReducer) % 2 == 0) {
+      player4.appendChild(cockShuttle);
+    }
+    else {
+      player3.appendChild(cockShuttle);
+    }
+    } 
+function negativeScoreAlert(){
+  var spanScoreT1 = document.querySelector('#score1');
+  var spanScoreT2 = document.querySelector('#score2');
+    if( parseInt(spanScoreT1.innerHTML)< 0 ){ 
+        alert(`Score cannot be under 0 ! `);
+        spanScoreT1.innerHTML = 0;
+    }
+    else if (parseInt(spanScoreT2.innerHTML)< 0){
+     alert(`Score cannot be under 0 ! `);
+        spanScoreT2.innerHTML = 0;
+    }
+}
 function cockCreate(){
     var score1T = document.getElementById('score1'); 
     var score2T = document.getElementById('score2');
     var starter = document.getElementById('starterButt');
     var pointAddbuttons = document.querySelectorAll('.butt');
-    console.log(pointAddbuttons);
-     pointAddbuttons[0].style.visibility = 'visible';
+        pointAddbuttons[0].style.visibility = 'visible';
      pointAddbuttons[1].style.visibility = 'visible';
     
    var cockImg = document.createElement('div');
@@ -109,24 +161,23 @@ function cockCreate(){
 }
 function winnerOfSet(){
   var score1T = document.getElementById('score1').innerHTML;
-  var score2T = document.getElementById('score2').innerHTML;
-  var paraWinnerInfo = document.querySelector('#pinfowin');
-    
+  var score2T = document.getElementById('score2').innerHTML; 
   var winner, loser;
  if (parseInt(score1T) - parseInt(score2T) >= 2 && parseInt(score1T) >= 21 || parseInt(score1T) == 30 )
 {
-    winner = ' kokoti from Team 1';
+    winner = ' kokoti from team 1';
     loser = ' team 2';
      gridContainer.appendChild(divWinn);
      paraWinnerInfo.innerHTML = `${winner} has won`;
  }
    if (parseInt(score2T) - parseInt(score1T) >= 2 && parseInt(score2T) >= 21 || parseInt(score2T) == 30 )
  { 
-     winner = 'kokoti from Team 2';
+     winner = 'kokoti from team 2';
       loser = 'team 1';
   gridContainer.appendChild(divWinn);
  paraWinnerInfo.innerText = `${winner} has won`;
     }
+    divWinn.appendChild(paraWinnerInfo);
   }
 function nextSet(){
   var score1T = document.getElementById('score1');
@@ -146,12 +197,12 @@ var rstButt = document.createElement('button');
 rstButt.classList.add('butt');
 rstButt.innerText = 'restart match ?';
 rstButt.addEventListener('click',nextSet);
-/*rstButt.addEventListener('click',function(){
+rstButt.addEventListener('click',function(){
     gridContainer.removeChild(divWinn);
    var cockShuttle = document.getElementById('cock');
     document.removeChild(cockShuttle);
 }
-);*/
+);
 divWinn.appendChild(rstButt);
 
 //game container
@@ -159,7 +210,6 @@ var gameContainer = document.createElement('div');
 gameContainer.classList.add('game');
 gridContainer.appendChild(gameContainer);
 var player1 = document.createElement('div');
-//player1.setAttribute('name','player1');
 player1.setAttribute('id','oneer')
 player1.classList.add('players');
 player1.innerText = 1;
